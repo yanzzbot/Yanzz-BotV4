0989775aca38e12abad57c074ba005e60fe8324e
yanzzbot <77144968+yanzzbot@users.noreply.github.com>
2021-01-15 22:24:40 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,70 @@
+const help = (prefix) => { 
+	return `
+╭══─⊱  ⸨ 𝐼𝑁𝐹𝑂 ⸩  ⊰─══╮
+║
+┣╾⊱ * DARK BOT*
+┣╾⊱ *9.0*
+┣╾⊱ *VERSION : TERMUX*
+┣╾⊱ *wa.me/6281368646011*
+┣╾⊱ *THANKS TO :*
+┣━━━━[ *RIZKY* ]━━━━━
+┣━━━━[ *CALIPH* ]━━━━
+┣━━━━[ *DENIS* ]━━━━━
+║
+╰══─⊱  ⸨ ANKER ⸩  ⊰─══╯
+     𝐑𝐮𝐥𝐞𝐬 - 𝐒𝐢𝐦𝐩𝐥𝐞
+▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬
+●⧐JANGAN SPAM BOT
+●⧐JANGAN CALL.VC BOT
+●⧐LANGGAR GW BAND
+▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬
+▣═─⊱【 𝑴𝑬𝑵𝑼 𝑺𝑰𝑴𝑷𝑳𝑬 】⊰─══
+║
+╰─⊱ 
+   |─⊱ *${prefix}listmenu*
+╭─⊱
+║
+▣═─⊱【 𝐿𝐼𝑆𝑇 𝑀𝐸𝑁𝑈 】⊰─══
+║
+╰─⊱ *${prefix}ownermenu*
+╭─⊱ *${prefix}adminmenu*
+  |─⊱ *${prefix}nsfwmenu*
+  |─⊱ *${prefix}funmenu*
+  |─⊱ *${prefix}mediamenu*
+  |─⊱ *${prefix} makermenu*
+  |─⊱ *${prefix}vipmenu [KHUSUS VIP!!]*
+  |─⊱ *${prefix}kerangmenu*
+╰─⊱ *${prefix}animemenu*
+╭─⊱ *${prefix}othermenu*
+║
+▣═══─⊱【 𝑂𝑇𝐻𝐸𝑅 】⊰─═══
+║
+╰─⊱ *${prefix}request [teksmu*
+╭─⊱ *${prefix}setprefix*
+  |─⊱ *${prefix}bugreport [teksmu]*
+  |─⊱ *${prefix}listblock*
+  |─⊱ *${prefix}iklan*
+  |─⊱ *${prefix}runtime*
+  |─⊱ *${prefix}info*
+  |─⊱ *${prefix}rules*
+  |─⊱ *${prefix}tnc*
+  |─⊱ *${prefix}cekvip*
+  |─⊱ *${prefix}daftarvip*
+  |─⊱ *${prefix}addvip*
+  |─⊱ *${prefix}dellvip*
+  |─⊱ *${prefix}snk*
+  |─⊱ *${prefix}darkadmin*
+  |─⊱ *${prefix}darkgroup*
+  |─⊱ *${prefix}listpremium*
+  |─⊱ *${prefix}donate*
+╰─⊱ *${prefix}ping*
+╭─⊱ *${prefix}owner*
+║
+▣══─⊱ 【 𝑅𝑈𝑁𝑇𝐼𝑀𝐸 】 ⊰─══
+║
+╰─⊱ *STATUS BOT: Online*
+╭─⊱ BOT ON: *TERGANTUNG OWNER*
+║
+▣══─ ⸨ DARK BOT ⸩ ─══▣`
+}
+exports.help = help
